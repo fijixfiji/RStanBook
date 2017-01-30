d704b7bcc18c616a178409650f0a71aca090d669
fijixfiji <fijixfiji+github@gmail.com>
2017-01-30 16:42:13 +0900

    rstan-extract-MCMCsamplesをPythonへ書き換え

--- chap04/pystan-extract-MCMCsamples.py
+++ chap04/pystan-extract-MCMCsamples.py
@@ -1,7 +1,20 @@
-load('output/result-model4-5.RData')
+#! -*- coding: utf-8 -*-
+import pickle
+import numpy as np
 
-ms <- rstan::extract(fit)
-N_mcmc <- length(ms$lp__)
-y50_base <- ms$a + ms$b * 50
-y50 <- rnorm(n=N_mcmc, mean=y50_base, sd=ms$sigma)
-d_mcmc <- data.frame(a=ms$a, b=ms$b, sigma=ms$sigma, y50_base, y50)
+# load('output/result-model4-5.RData')
+
+# ms <- rstan::extract(fit)
+# N_mcmc <- length(ms$lp__)
+# y50_base <- ms$a + ms$b * 50
+# y50 <- rnorm(n=N_mcmc, mean=y50_base, sd=ms$sigma)
+# d_mcmc <- data.frame(a=ms$a, b=ms$b, sigma=ms$sigma, y50_base, y50)
+
+with open('output/result-model4-5.pickle', 'rb') as f:
+    stanmodel = pickle.load(f)
+    fit = pickle.load(f)
+
+ms = fit.extract()
+N_mcmc = len(ms['lp__'])
+y50_base = ms['a'] + ms['b'] * 50
+y50 = np.random.normal(loc=y50_base, scale=ms['sigma'])
