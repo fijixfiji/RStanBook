f6683f49d3594ebfc2854e6db906e58b6a0b636d
fijixfiji <fijixfiji+github@gmail.com>
2017-01-30 16:39:32 +0900

    fitを保存するために、先にstanmodelを保存するようにした

--- chap04/run-model4-5.py
+++ chap04/run-model4-5.py
@@ -13,10 +13,11 @@ import pystan
 
 d = np.genfromtxt(fname='input/data-salary.txt', delimiter=',', names=True, dtype=np.float)
 data = {'N': d.size, 'X': d['X'], 'Y': d['Y']}
-fit = pystan.stan(file='model/model4-5.stan', data=data, seed=1234)
+stanmodel = pystan.StanModel(file='model/model4-5.stan')
+fit = stanmodel.sampling(data=data, seed=1234)
 
 with open('output/result-model4-5.pickle', 'wb') as f:
-    # fitではなくて、stanmodelとfit.extract()を保存すべき？
+    pickle.dump(stanmodel, f, protocol=2)
     pickle.dump(fit, f, protocol=2)
 
 # print(fit) # 4.4.7の出力
